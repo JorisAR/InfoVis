62adaeb7a1d0fc84ca0664f43fc90cfa1131d18c
Joris Rijsdijk <j.a.rijsdijk@student.tudelft.nl>
2023-12-11 18:54:55 +0100

    fix it

--- stream_graph/stream_graph.js
+++ stream_graph/stream_graph.js
@@ -27,7 +27,6 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
-// Parse the Data
 d3.csv("../data/spotify_songs.csv", function(d) {
     // Convert quantitative scales to floats
     for (var k in d) {
@@ -39,7 +38,6 @@ d3.csv("../data/spotify_songs.csv", function(d) {
   }, function(error, data) {
     if (error) throw error;
 
-    // Create the stack layout
     // Group data by genre and track_album_release_date
     var nestedData = d3.nest()
     .key(function(d) { return d.playlist_genre; })  // Group data by genre
@@ -48,21 +46,19 @@ d3.csv("../data/spotify_songs.csv", function(d) {
         // For each unique genre and year combination, sum the track_popularity
         return d3.sum(v, function(d) { return d.track_popularity; }); 
     })
-    .entries(data);  // Apply this to your data
-
+    .entries(data);
 
     console.log("nestedData")
     console.log(nestedData)
 
     // Transform the nested data into a suitable format for d3.layout.stack()
-    var stackedData = nestedData.reduce(function(acc, d) {
+    var dataPerYear = nestedData.reduce(function(acc, d) {
         d.values.forEach(function(v) {
             var existing = acc.find(function(e) { return e.x === v.key; });
             if (existing) {
                 existing[d.key] = v.value;
             } else {
                 var newObj = {x: v.key};
-                // Ensure newObj has a property for each genre
                 Object.keys(genre_colors).forEach(function(genre) {
                     newObj[genre] = genre === d.key ? v.value : 0;
                 });
@@ -72,38 +68,65 @@ d3.csv("../data/spotify_songs.csv", function(d) {
         return acc;
     }, []);
 
-    
-    // Create the stack layout
-    var stack = d3.stack()
-        .offset(d3.stackOffsetSilhouette)
-        .keys(Object.keys(genre_colors))
-        //.value(function(d, key) { return d[key] || 0; });
-    
-    var layers = stack(stackedData);
+    dataPerYear.sort(function(a, b) {
+        return a.x - b.x;
+    });
 
+    // First, calculate the total for each year
+    var totalsPerYear = {};
+    dataPerYear.forEach(function(d) {
+        var total = 0;
+        Object.keys(genre_colors).forEach(function(genre) {
+            total += d[genre];
+        });
+        totalsPerYear[d.x] = total;
+    });
+
+    // Then, normalize the values
+    dataPerYear.forEach(function(d) {
+        Object.keys(genre_colors).forEach(function(genre) {
+            d[genre] = d[genre] / totalsPerYear[d.x];
+        });
+    });
+
+    // console.log("dataPerYear")
+    // console.log(dataPerYear)
 
     // Calculate the maximum total track_popularity across all genres for any given year
-    var maxPopularity = d3.max(stackedData, function(d) {
+    var maxPopularity = d3.max(dataPerYear, function(d) {
         var total = 0;
         for (var genre in genre_colors) {
             total += d[genre] || 0;
         }
         return total;
     });
+    console.log("max popularity: " + maxPopularity)
+    
+    var stack = d3.stack()
+        //.offset(d3.stackOffsetNone)//d3.stackOffsetSilhouette
+        .offset(d3.stackOffsetNone)
+        .keys(Object.keys(genre_colors))
+        // .value(function(d, key) { return d[key] || 0; });
+    
+    var stackedData = stack(dataPerYear);
+    console.log("stackedData")
+    console.log(stackedData)
+
     // Define the x, y, and color scales
-    var x = d3.scaleLinear()  // Change from scaleTime to scaleLinear
+    var x = d3.scaleLinear()
         .domain(d3.extent(data, function(d) { return new Date(d.track_album_release_date).getFullYear(); }))
         .range([0, width]);
     var y = d3.scaleLinear()
         .domain([0, maxPopularity])
-        .range([height, 0]);
+        .range([height * .78, height * .02]);
     
-        var color = function(genre) {
-            return hsv_to_color(genre);
-        }
+    var color = function(genre) {
+        return hsv_to_color(genre);
+    }
+
     svg.append("g")
         .attr("transform", "translate(0," + height*0.8 + ")")
-        .call(d3.axisBottom(x).tickSize(-height*.7).tickValues([new Date(1980, 0, 1), new Date(1990, 0, 1), new Date(2000, 0, 1), new Date(2010, 0, 1)]))
+        .call(d3.axisBottom(x).tickSize(-height*.8).ticks(5))
         .select(".domain").remove()
     // Customization
     svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
@@ -111,11 +134,26 @@ d3.csv("../data/spotify_songs.csv", function(d) {
     svg.append("text")
         .attr("text-anchor", "end")
         .attr("x", width)
-        .attr("y", height-30 )
-        .text("Time (year)");
+        .attr("y", height * .9)
+        .text("Time (year)");    
 
+    const area = d3.area()
+        .x((d, i) => x(d.data.x)) //console.log(x(d.data.x)); return i / 61 * width;
+        .y0(d => y(d[0]))
+        .y1(d => y(d[1]));
 
-    console.log(layers)
+    svg
+        .selectAll("mylayers")
+        .data(stackedData)
+        .enter()
+        .append("path")
+          .attr("class", "myArea")
+          .style("fill", function(d) { return color(d.key); })
+          .attr("d", area)
+        //   .on("mouseover", mouseover)
+        //   .on("mousemove", mousemove)
+        //   .on("mouseleave", mouseleave)
+});
 
     // var mouseover = function(d) {
     //     Tooltip.style("opacity", 1)
@@ -132,21 +170,3 @@ d3.csv("../data/spotify_songs.csv", function(d) {
     //     Tooltip.style("opacity", 0)
     //     d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none")
     // }
-
-    // Update the area generator and the areas
-    var area = d3.area()
-    .x(function(d) { return x(d.data.x); })  // change from d.track_album_release_date to d.data.x
-    .y0(function(d) { return y(d[0]); })  // change from d.y0 to d[0]
-    .y1(function(d) { return y(d[1]); });  // change from d.y + d.y0 to d[1]
-    svg
-        .selectAll("mylayers")
-        .data(layers)
-        .enter()
-        .append("path")
-          .attr("class", "myArea")
-          .style("fill", function(d) { return color(d.key); })
-          .attr("d", area)
-        //   .on("mouseover", mouseover)
-        //   .on("mousemove", mousemove)
-        //   .on("mouseleave", mouseleave)
-});
