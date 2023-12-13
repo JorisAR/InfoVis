049389b88bc94e3b996c21b22e33c49d9e8e0047
JARijsdijk <j.a.rijsdijk@student.tudelft.nl>
2023-12-13 08:56:56 +0100

    increase width

--- stream_graph/stream_graph.js
+++ stream_graph/stream_graph.js
@@ -15,7 +15,7 @@ var genre_colors = {
 
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 30, bottom: 0, left: 10},
-    width = 460 - margin.left - margin.right,
+    width = 980 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
